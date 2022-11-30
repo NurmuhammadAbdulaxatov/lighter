338a455cd1c9bb05cc8ac85ed7a0d74a2b19e714
Nurmuhammad <abdulaxatov3@gmail.com>
2022-11-30 17:14:15 +0500

    background qowildi

--- js/app.js
+++ js/app.js
@@ -9,4 +9,14 @@ setInterval(() => {
   } else {
     slide.setAttribute("src", "images/OFFbulb.jpg");
   }
-}, 2000);
+}, 1000);
+
+let background = document.querySelector("body");
+
+setInterval(() => {
+  let red = Math.floor(Math.random() * 255) + 1;
+  let green = Math.floor(Math.random() * 255) + 1;
+  let blue = Math.floor(Math.random() * 255) + 1;
+
+  background.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+}, 200);
